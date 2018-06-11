313ea4eee293293f0b9693df8c208d1692f5da6a
Xiaomin Huang <huangchenxiaomin@gmail.com>
2018-06-10 23:41:26 -0400

    Added test for diagonal of first and second order DM.

--- var2DM/test/test_misc.py
+++ var2DM/test/test_misc.py
@@ -4,7 +4,38 @@ from var2DM import misc
 from var2DM import tools
 import gbasis as gb
 import iodata as iod
+import numpy as np
+import numpy.testing as npt
 import os
+
+def test_diag_first_dm():
+    """Test that the diagonal of the first order density matrix adds to 
+       the number of electrons.
+
+    """
+    nelec = 8
+    norb = nelec + 10
+    first_dm = first_dm_hf(norb, nelec)
+    trace = np.trace(first_dm)
+    msg = "Diagonal of first order DM does not equal N"
+    exp_trace = nelec
+    npt.assert_allclose(trace, exp_trace, err_msg=msg)
+
+
+def test_diag_second_dm():
+    """Test that the diagonal of the second order density matrix adds to 
+       the N*(N-1).
+
+    """
+    nelec = 8
+    norb = nelec + 10
+    first_dm = first_dm_hf(norb, nelec)
+    second_dm = second_dm_hf(first_dm, norb, nelec)
+    trace = np.trace(second_dm)
+    msg = "Diagonal of second order DM does not equal N * (N - 1)"
+    exp_trace = nelec * (nelec - 1)
+    npt.assert_allclose(trace, exp_trace, err_msg=msg)
+
 
 def test_prep_electron_int():
     test_file = tools.get_xyz_path('water.xyz')
