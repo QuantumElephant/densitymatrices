68cdcd1655d958c6daba70aa8ed90f42566f9d8a
Xiaomin Huang <huangchenxiaomin@gmail.com>
2018-06-14 17:08:59 -0400

    Minor fixes for first and second DM stuff.

--- var2DM/test/test_misc.py
+++ var2DM/test/test_misc.py
@@ -8,14 +8,15 @@ import numpy as np
 import numpy.testing as npt
 import os
 
+
 def test_diag_first_dm():
-    """Test that the diagonal of the first order density matrix adds to 
+    """Test that the diagonal of the first order density matrix adds to
        the number of electrons.
 
     """
     nelec = 8
     norb = nelec + 10
-    first_dm = first_dm_hf(norb, nelec)
+    first_dm = misc.first_dm_hf(norb, nelec)
     trace = np.trace(first_dm)
     msg = "Diagonal of first order DM does not equal N"
     exp_trace = nelec
@@ -23,14 +24,14 @@ def test_diag_first_dm():
 
 
 def test_diag_second_dm():
-    """Test that the diagonal of the second order density matrix adds to 
+    """Test that the diagonal of the second order density matrix adds to
        the N*(N-1).
 
     """
     nelec = 8
     norb = nelec + 10
-    first_dm = first_dm_hf(norb, nelec)
-    second_dm = second_dm_hf(first_dm, norb, nelec)
+    first_dm = misc.first_dm_hf(norb, nelec)
+    second_dm = misc.second_dm_hf(first_dm, norb, nelec)
     trace = np.trace(second_dm)
     msg = "Diagonal of second order DM does not equal N * (N - 1)"
     exp_trace = nelec * (nelec - 1)
@@ -43,7 +44,7 @@ def test_prep_electron_int():
     # note: coordinates are in bohr not angstroms
     mol = iod.IOData.from_file(test_file)
     misc.prep_electron_int(mol.coordinates, mol.numbers,
-                      mol.pseudo_numbers, '3-21G')
+                           mol.pseudo_numbers, '3-21G')
 
 
 if __name__ == "__main__":
